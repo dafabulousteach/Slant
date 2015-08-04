4c3ad1cc7c5dd669c8a15362f21b5d5a073159be
Kim Merino <dafabulousteach@gmail.com>
2015-08-03 17:16:45 -0700

    removed karma files added dependencies added icon in toolbar

---
+++ app.js
@@ -0,0 +1,13 @@
+// angular.module('slant.auth', [])
+// .controller('AppController', function(){
+// 	$scope.score = {};
+
+// 	$scope.getScore = function(){
+// 		console.log('The score is 4');
+// 	};
+// });
+
+// chrome.browserAction.onClicked.addListener(function(activeTab)
+// {
+//     $(this).text('The icon has been clicked');
+// });
