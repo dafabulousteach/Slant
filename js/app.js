07632182654eadf644336a1535ade5337363a248
TGA-Student <tga.student@hackreactor.com>
2015-08-04 16:52:41 -0700

    alerts the user of the slant score the news site

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 var getURL = function(tabURL){
   var newsArray = window.newsSites;
-  var str = 'The slant score is: ';
+  var str = 'The slant score of this news site is: ';
   for(var i = 0; i < newsArray.length; i++){
     if(newsArray[i]['url'] === tabURL){
      var match = newsArray[i]['score'];
     }
   }
   var message = str + '' + match;
-
-  console.log(message);
+  alert(message);
 };
 
 
 chrome.browserAction.onClicked.addListener(function(tab){
-  var newURL = "/popups/index.html";
-  chrome.tabs.create({ url: newURL});
 
-  // chrome.tabs.query({currentWindow: true, active: true},  function(tabs){
-  //   var tabURL = tabs[0].url;
-  //   getURL(tabURL);
+  chrome.tabs.query({currentWindow: true, active: true},  function(tabs){
+    var tabURL = tabs[0].url;
+    getURL(tabURL);
+  // var newURL = "/popups/index.html";
+  // chrome.tabs.create({ url: newURL});
   // });
+
 });
 
 chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
